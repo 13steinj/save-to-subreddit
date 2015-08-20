8046b223ec0c218b4b6484e5dd5fcaf8be95c935
13steinj <13steinj@users.noreply.github.com>
2015-08-20 01:20:05 -0400

    Compatibility Update + Format
    Hey alkorin, I made your script compatible with both python 2 and python 3 (I think. Run this script with a test account first just in case, run in both Python2 and 3).
    
    I also added to forms of preemptive user configuration, as well as removed most human error out of ignorance (starting the username with u/ or /u/ or their capital variants won't fuck things up. This is the same with the Subreddit, using the /r/variants are no longer a , and I made the limit for getting the posts 25 by default, just in case a user makes a mistake. The user can set other options.
    
    
    I also made it PEP257 Compliant.
    
    Part of this was removing the `saved` empty list and `saved_dict` empty dictionary, as in reality you didn't use them.
    
    Before merging, test it with an alt account on both python 2 and 3. I still will later add the private Subreddit enforcement and OAuth compatibility

--- RedditSaved.py
+++ RedditSaved.py
@@ -1,28 +1,76 @@
 import praw
+from six.moves import input
 
-saved = []
-save_dict = {}
-r = praw.Reddit(user_agent = "save-to-subreddit")
-username = raw_input(">>Username:")
-password = raw_input(">>Password:")
-subreddit = raw_input(">>Subreddit (without /r/)")
+# Python Reddit API Wrapper
+# Python 2/3 Compatability library. If the user has PRAW,
+# the user has six, as six is installed with PRAW
 
-r.login(username,password)
+### USER CONFIGURATION ###
+USERNAME = ""
+PASSWORD = ""
+SUBREDDIT = ""
 
-for post in r.user.get_saved():
+# If LIMIT is < or = to 0, the amount will be the amount
+# of posts you see in a page on your browser. If 'None'
+# without quotes, it will do ALL saved posts. If > than
+# 0 and < than 101, the amount will be that number.
 
-    post_name = str(post)
+LIMIT = 25
 
-    try:
-        semi = post_name.index(':')
-        post_name = post_name[semi+2:]
-    except Exception, CannotFind:
-        pass
+# If "botconfig.py" is in the folder or Python Library, the
+# script will overwrite the above configuration with the
+# equivalent variables in the module if the variable
+# exists for ease of access, safety, and redistribution  
 
-    sub = str(post.subreddit.display_name)
-    package = '[' + sub + ']' + ' ' + post_name
-    link = post.permalink
-    print package
-    r.submit(subreddit, package, url = link)
-    post.unsave()
+try:
+    import botconfig
+    USERNAME = botconfig.username
+    PASSWORD = botconfig.password
+    SUBREDDIT = botconfig.subreddit
+except ImportError:
+    pass
+except AttributeError:
+    pass
 
+
+def main():
+    r = praw.Reddit(user_agent = "save-to-subreddit")
+    if USERNAME == "":
+        USERNAME = input(">>Username: ")
+    if PASSWORD == "":
+        PASSWORD = input(">>Password: ")
+    if SUBREDDIT == ""
+        SUBREDDIT = input(">>Subreddit: ")
+    USERNAME.replace("/u/", "")
+    USERNAME.replace("/U/", "")
+    USERNAME.replace("u/", "")
+    USERNAME.replace("U/", "")
+    SUBREDDIT.replace("/r/", "")
+    SUBREDDIT.replace("/R/", "")
+    SUBREDDIT.replace("r/", "")
+    SUBREDDIT.replace("R/", "")
+    USERAGENT = "Save-to-Subreddit by /u/alkorin"
+    if USERNAME != "alkorin":
+        USERAGENT += " in use by {0}".format(USERNAME)
+    r = praw.Reddit(USERAGENT)
+    r.login(USERNAME, PASSWORD)
+
+    for post in r.get_me().get_saved(limit=LIMIT):
+
+        post_name = str(post)
+
+        try:
+            semi = post_name.index(':')
+            post_name = post_name[semi+2:]
+        except Exception, CannotFind:
+            pass
+
+        sub = str(post.subreddit.display_name)
+        package = '[' + sub + ']' + ' ' + post_name
+        link = post.permalink
+        print package
+        r.submit(subreddit, package, url=link)
+        post.unsave()
+
+if __name__ == '__main__':
+    main()
